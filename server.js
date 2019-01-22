615aaa0f1d4b80e51892505e3ebcb6457cef6d73
Junior Silva <jros2.silva@gmail.com>
2019-01-22 00:35:46 -0300

    Arquivos de rota e express router

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/user.js');
+const profile = require('./routes/api/profile.js');
+const posts = require('./routes/api/post.js');
 
 const app  = express();
 
+const db = require('./config/keys').mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(()=>console.log('Mogodb connected'))
+    .catch(error => console.log(error));
+
+
 app.get('/', (req,res)=>res.send("Hello"));
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
